e898f96fea384dbf0a3e37cf093c707e76dbd467
njacobs3 <njacobs3@illinois.edu>
2024-11-15 09:42:26 -0600

    Added procedure selection screen/some functionality

--- app/src/main/java/com/example/atlasinstructionsskeleton/MainActivity.java
+++ app/src/main/java/com/example/atlasinstructionsskeleton/MainActivity.java
@@ -161,6 +161,11 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     voidOpenMenu();
+
+                    currentSlideIndex =0;
+
+                    updateSlide();
+                    resetProgress();
                     dialog.dismiss();
 
                 }
